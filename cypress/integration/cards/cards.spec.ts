4e271ae3ed2104afd76bae66680c7e722dc99bdc
Anthony Giang <anthonyhgiang@gmail.com>
2022-02-15 20:51:55 -0800

    Update cards component test to utilize http client

--- cypress/integration/cards/cards.spec.ts
+++ cypress/integration/cards/cards.spec.ts
@@ -1,9 +1,17 @@
-//import { Card } from '../../../src/app/card'
+import { Card } from '../../../src/app/card'
 
 describe('Test cards-component', () => {
     it('Test cards-component is displayed"', () => {
-        cy.visit('/cards')
-        cy.fixture('cards/cards').then((cards) => {
+        cy.fixture('cards/cards').then((cards: Card[]) => {
+            cy.intercept('GET', Cypress.env('apiCards'), {
+                statusCode: 200,
+                body: cards
+            }).as('get-cards')
+
+            cy.visit('/cards')
+            cy.wait('@get-cards')
+
+            // Test that cards are displayed
             for(let i=0; i<cards.length; i++) {
                 cy.get('ul.cards > li').eq(i).within(() => {
                     cy.get('#card-name').should('have.text', cards[i].name)
